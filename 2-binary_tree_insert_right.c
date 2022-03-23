2203776dfa38390a39e855df2991a35e4bf3b6b9
Cecales <cesar.caleroes@gmail.com>
2022-03-23 11:55:58 -0500

    Test point 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,8 +1,39 @@
 #include "binary_trees.h"
 
 /**
- * kfkdsjf
- * 
+ * binary_tree_insert_right - This file insert a node as
+ * right child of a parent node
+ * @parent: pointer to the parent node of the node define as right child
+ * @value: value to put in the new node
+ * Return: new node or NULL in failure
  */
 
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
 
+	if (!parent)
+		return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->left = NULL;
+
+	if (parent->right == NULL)
+	{
+		new_node->parent = parent;
+		new_node->right = NULL;
+		parent->right = new_node;
+	}
+	else
+	{
+		new_node->parent = parent;
+		new_node->right = parent->right;
+		parent->right = new_node;
+		new_node->right->parent = new_node;
+	}
+	return (new_node);
+}
