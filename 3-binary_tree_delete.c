53c031aea6eed284d0a2d335e9c6954d68cf898e
Cecales <cesar.caleroes@gmail.com>
2022-03-23 17:13:33 -0500

    Test point 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,8 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * kfkdsjf
- * 
+ * binary_tree_delete - This file delete an entire binary tree
+ * @tree: pointer to the root node of the binary tree to delete
+ * Return: nothing if tree is NULL
  */
 
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
 
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
