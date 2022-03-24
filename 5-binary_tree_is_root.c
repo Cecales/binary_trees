71f41060f027721f08ce1d82b4cad1f805f5feed
Cecales <cesar.caleroes@gmail.com>
2022-03-24 16:19:37 -0500

    Test more points

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,8 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * kfkdsjf
- * 
+ * binary_tree_is_root - This file check if a node is a root node
+ * @node: pointer to the node to check
+ * Return: If the node is root returns 1,
+ * if the node is not root or null return 0
  */
 
-
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node && !node->parent)
+		return (1);
+	return (0);
+}
