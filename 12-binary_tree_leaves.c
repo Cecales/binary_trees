b8759c73404f042b6845105bee44b9a7a5791dda
Cecales <cesar.caleroes@gmail.com>
2022-03-24 22:57:06 -0500

    Upload and check point 13

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,8 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * kfkdsjf
- * 
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: 0 If tree is NULL
+ * A NULL pointer is not a leaf
  */
 
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
 
+	if (!tree)
+		return (0);
+	if (!(tree->left) && !(tree->right))
+		return (1);
+
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+
+	return (leaves);
+}
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,8 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * kfkdsjf
- * 
+ * binary_tree_nodes - function that counts the nodes
+ * with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to
+ * count the number of nodes
+ * Return: 0 If tree is NULL
+ * A NULL pointer is not a node
  */
 
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t cont = 0;
 
+	if (!tree)
+		return (0);
+
+	if (tree->left || tree->right)
+		cont = 1;
+
+	cont += binary_tree_nodes(tree->left);
+	cont += binary_tree_nodes(tree->right);
+
+	return (cont);
+}
