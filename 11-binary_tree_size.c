288ecbff9638b5ac30c5694ab7bd2977371a2604
Cecales <cesar.caleroes@gmail.com>
2022-03-24 22:17:16 -0500

    Test more points

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,8 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * kfkdsjf
- * 
+ * binary_tree_size -  function that measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: 0 If tree is NULL
  */
 
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t siz = 1;
 
+	if (!tree)
+		return (0);
+
+	siz += binary_tree_size(tree->left);
+	siz += binary_tree_size(tree->right);
+
+	return (siz);
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,8 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * kfkdsjf
- * 
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: 0 If tree is NULL, or the value of the height
  */
 
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_h = 0, right_h = 0;
 
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left_h = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_h = 1 + binary_tree_height(tree->right);
+
+	if (left_h > right_h)
+		return (left_h);
+	return (right_h);
+}
